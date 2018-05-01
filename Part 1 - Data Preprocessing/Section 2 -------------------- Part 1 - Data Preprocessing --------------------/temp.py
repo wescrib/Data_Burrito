8b10d5d01f8b170f4fca9ec9b2473593e0c38472
wescrib <scribner.william@gmail.com>
2018-04-30 20:52:51 -0400

    feature scaled stuff.. not 100% sure

--- Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/temp.py
+++ Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/temp.py
@@ -55,4 +55,23 @@ y = labelencoder_y.fit_transform(y)
 
 from sklearn.cross_validation import train_test_split
 
-x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.3, random_state = 0)
+x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.2, random_state = 0)
+
+
+#FEATURE SCALING
+from sklearn.preprocessing import StandardScaler
+sc_x = StandardScaler()
+
+x_train = sc_x.fit_transform(x_train)
+x_test = sc_x.transform(x_test)
+
+
+
+
+
+
+
+
+
+
+
