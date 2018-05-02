be349512b2904c6ff284dee74e3f06073592825c
wescrib <scribner.william@gmail.com>
2018-05-01 22:56:27 -0400

    BUILT A GRAPHgit add --allgit add --allgit add --all

--- Part 2 - Regression/Section 4 - Simple Linear Regression/Python_Regression.py
+++ Part 2 - Regression/Section 4 - Simple Linear Regression/Python_Regression.py
@@ -24,8 +24,26 @@ regressor = LinearRegression()
 regressor.fit(x_train, y_train,)
 
 """regressor IS the machine that is learning. this is a super basic machin learning model."""
-"""training data is basically the data that the machine will learn from. the test data is where the learning will be applied. BAM!"""
+"""training data is basically the data that the machine will learn from. the test data is where the learning will be applied, i.e. THE PREDICTION. BAM!"""
 
 #WILL PREDICT TEST SET RESULTS
 y_predict = regressor.predict(x_test)
 """after this line, compare y_test data and the new y_predict data | y_test is legit real data"""
+
+#VISUALIZATION OF TRAINING DATA
+"""matplotlib is finally making its appearance"""
+plt.scatter(x_train, y_train, color='red')
+plt.plot(x_train,regressor.predict(x_train), color='green')
+plt.title("SALARY vs EXPERIENCE (Training Data)")
+plt.xlabel("Years of Experience")
+plt.ylabel("Salary")
+plt.show()
+
+#VISUALIZATION OF TEST DATA
+"""matplotlib is finally making its appearance"""
+plt.scatter(x_test, y_test, color='red')
+plt.plot(x_train,regressor.predict(x_train), color='green')
+plt.title("SALARY vs EXPERIENCE (Training Data)")
+plt.xlabel("Years of Experience")
+plt.ylabel("Salary")
+plt.show()
