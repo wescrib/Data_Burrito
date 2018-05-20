11ea5cb32095b3bcc578b9dd2a5a016630462a6d
wescrib <scribner.william@gmail.com>
2018-05-20 16:52:22 -0400

    completed template

--- Part 2 - Regression/Section 6 - Polynomial Regression/data_preprocessing_template.py
+++ Part 2 - Regression/Section 6 - Polynomial Regression/data_preprocessing_template.py
@@ -7,12 +7,15 @@ import pandas as pd
 
 # Importing the dataset
 dataset = pd.read_csv('Data.csv')
-X = dataset.iloc[:, :-1].values
+x = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 3].values
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 0)
+
+#Fitting regression model to dataset
+#regressor goes here
 
 # Feature Scaling
 """from sklearn.preprocessing import StandardScaler
@@ -21,3 +24,16 @@ X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
 sc_y = StandardScaler()
 y_train = sc_y.fit_transform(y_train)"""
+
+y_pred = regressor.predict(6.5)
+
+#visualize data
+"""does the plotting, literally x and y coords"""
+x_grid = np.arange(min(x),max(x),0.1)
+x_grid = x_grid.reshape((len(x_grid),1))
+plt.scatter(x, y, color = 'red')
+plt.plot(x_grid, regressor.predict(x_grid), color='blue')
+plt.title("Truth or Bluff (Poly Regression)")
+plt.xlabel("Position Level")
+plt.ylabel("Salary")
+plt.show()
