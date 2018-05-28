79cf81062848dbb6c1213b858bb6561685d86474
wescrib <scribner.william@gmail.com>
2018-05-27 23:37:00 -0400

    SVR section completed

--- Part 2 - Regression/Section 6 - Polynomial Regression/data_preprocessing_template.py
+++ Part 2 - Regression/Section 6 - Polynomial Regression/data_preprocessing_template.py
@@ -6,34 +6,37 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv('Data.csv')
-x = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, 3].values
+dataset = pd.read_csv('Position_Salaries.csv')
+x = dataset.iloc[:, 1:2].values
+y = dataset.iloc[:, 2].values.reshape(-1,1)
 
 # Splitting the dataset into the Training set and Test set
+"""
 from sklearn.cross_validation import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 0)
+"""
 
-#Fitting regression model to dataset
-#regressor goes here
 
 # Feature Scaling
-"""from sklearn.preprocessing import StandardScaler
-sc_X = StandardScaler()
-X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.transform(X_test)
+from sklearn.preprocessing import StandardScaler
+sc_x = StandardScaler()
 sc_y = StandardScaler()
-y_train = sc_y.fit_transform(y_train)"""
+x = sc_x.fit_transform(x)
+y = sc_y.fit_transform(y)
 
-y_pred = regressor.predict(6.5)
+#Fitting regression model to dataset
+from sklearn.svm import SVR
+regressor = SVR(kernel = 'rbf')
+regressor.fit(x, y)
+
+#predict new results
+y_pred = sc_y.inverse_transform(regressor.predict(sc_x.transform(np.array([[6.5]]))))
 
 #visualize data
 """does the plotting, literally x and y coords"""
-x_grid = np.arange(min(x),max(x),0.1)
-x_grid = x_grid.reshape((len(x_grid),1))
 plt.scatter(x, y, color = 'red')
-plt.plot(x_grid, regressor.predict(x_grid), color='blue')
-plt.title("Truth or Bluff (Poly Regression)")
+plt.plot(x, regressor.predict(x), color='blue')
+plt.title("Truth or Bluff (SVR)")
 plt.xlabel("Position Level")
 plt.ylabel("Salary")
 plt.show()
